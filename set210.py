6f0a9220fa0d67d743b3d311d22f6b56f3f93e0d
sujatha-2 <sujatha16081998@gmail.com>
2018-09-26 17:50:31 +0530

    Create set210.py

---
+++ set210.py
@@ -0,0 +1,33 @@
+print("TO FIND FIRST 5 MULTIPLES")
+   n=int(input("Enter the numbers"))
+for x in range(1,6):
+   m=n*x
+  print(m)
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
