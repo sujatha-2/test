565c75503c46758f5db00854adf516f5e58100c2
sujatha-2 <sujatha16081998@gmail.com>
2018-09-26 18:03:14 +0530

    Create set26.py

---
+++ set26.py
@@ -0,0 +1,11 @@
+print("DISPLAY PRIME NUMBERS BETWEEN 2 INTEGERS")
+  a=int(input("Enter the number")
+  b=int(input("Enter the number")
+     s=0
+for x in range(a+1,b):
+     s=0
+for x in range(1,x+1):
+    if(x%y==0):
+        s=s+1
+     if(s==2):
+       print(x)
