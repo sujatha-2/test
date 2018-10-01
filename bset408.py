2b6e3d6a248db1cfa308165c1955d3d8329d2f07
sujatha-2 <sujatha16081998@gmail.com>
2018-10-01 16:49:18 +0530

    Create bset408.py

---
+++ bset408.py
@@ -0,0 +1,19 @@
+a = 10
+b = 20
+ 
+print("before swapping\na=", a, " b=", b)
+ 
+temp = a
+a = b
+b = temp
+ 
+print("\nafter swapping\na=", a, " b=", b)a = 10
+b = 20
+ 
+print("before swapping\na=", a, " b=", b)
+ 
+temp = a
+a = b
+b = temp
+ 
+print("\nafter swapping\na=", a, " b=", b)
