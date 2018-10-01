740c69c79e87fe1c33befec7f40673cb6f86a194
sujatha-2 <sujatha16081998@gmail.com>
2018-10-01 17:12:50 +0530

    Create bset439.py

---
+++ bset439.py
@@ -0,0 +1,17 @@
+num1=15
+num2=4
+num3=5
+num4=6
+num5=7
+num6=8
+num7=1
+num8=2
+num9=6
+num10=5
+if(num 1>= num2) and (num 1>= num10):
+largest=num1
+elif(num 2>= num1) and (num2 >= num10):
+largest=num2
+else:
+         largest = num10 
+ print("The largest number between,"num1","num2", "and","num10" "is"largest)
