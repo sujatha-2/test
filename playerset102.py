50fa371683b449c211a8a676d8fcd06bc3ec1c14
sujatha-2 <sujatha16081998@gmail.com>
2018-10-04 16:42:43 +0530

    Create playerset102.py

---
+++ playerset102.py
@@ -0,0 +1,13 @@
+n=int(input("enter any number:"))
+fact=1
+while(n>1):
+fact=fact*n
+n=n-1
+print("the factorial of a number is:")
+print(fact)n=int(input("enter any number:"))
+fact=1
+while(n>1):
+fact=fact*n
+n=n-1
+print("the factorial of a number is:")
+print(fact)
