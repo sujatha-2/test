7b2659ce6a9093683f0b28f171253099478d8288
sujatha-2 <sujatha16081998@gmail.com>
2018-10-04 16:54:54 +0530

    Create playerset105.py

---
+++ playerset105.py
@@ -0,0 +1,12 @@
+romaninput=raw input("enter the roman number:")
+I=1
+V=5
+X=10
+L=50
+C=100
+D=500
+M=1000
+if(romaninput[0]>romaninput[1]):
+total=
+print("The roman numeral",romaninput,"is equal to",total)
+class solution:
