40e427c329d284da4762b4c17190f21683880919
sujatha-2 <sujatha16081998@gmail.com>
2018-10-04 17:37:10 +0530

    Create playerset110.py

---
+++ playerset110.py
@@ -0,0 +1,18 @@
+def nearlyEqual(string1, string2):
+  string1 = input('First Word')
+  string2 = input('Second Word')
+    if string1 == string2:
+      print (True)
+      else:
+      print (False)
+  return nearlyEqual(string1,string2)
+  print (nearlyEqual(string1,string2))
+string1 = ()
+string2 = ()
+print, nearlyEqual(string1, string2)else:
+      print (False)
+  return nearlyEqual(string1,string2)
+  print (nearlyEqual(string1,string2))
+string1 = ()
+string2 = ()
+print, nearlyEqual(string1, string2)
