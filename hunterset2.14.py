beedfd6ea2194c127875f15a2758f5d4c18957f3
sujatha-2 <sujatha16081998@gmail.com>
2018-10-10 17:11:34 +0530

    Create hunterset2.14.py

---
+++ hunterset2.14.py
@@ -0,0 +1,14 @@
+def toString(List): 
+    return ''.join(List) 
+    def permute(a, l, r): 
+    if l==r: 
+        print toString(a) 
+    else: 
+        for i in xrange(l,r+1): 
+            a[l], a[i] = a[i], a[l] 
+            permute(a, l+1, r) 
+            a[l], a[i] = a[i], a[l] 
+            string = "ABC"
+n = len(string) 
+a = list(string) 
+permute(a, 0, n-1)
