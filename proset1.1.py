8b96082c371b0a26941a099e40e145d09e838b7b
sujatha-2 <sujatha16081998@gmail.com>
2018-10-10 17:05:10 +0530

    Create proset1.1.py

---
+++ proset1.1.py
@@ -0,0 +1,10 @@
+print("Thelongestcommanprefix")
+a=int(input("\nEnter the number:"))
+b=input("\nEnter the string:")
+c=input("\nEnter the string:")
+d=0
+e=list(b)
+while a>0:
+    print(e[d],end="")
+    d=d+1
+    a=a-1
