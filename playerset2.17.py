cbe66d2d53066c8e192c5a73f611808f9a4eaa7b
sujatha-2 <sujatha16081998@gmail.com>
2018-10-11 17:43:06 +0530

    Create playerset2.17.py

---
+++ playerset2.17.py
@@ -0,0 +1,14 @@
+def countDivisibles(L,R,M): 
+counter = 0; 
+ for i in range(L,R): 
+        if (i % M == 0): 
+            counter = counter + 1
+  
+    return counter 
+     for i in range(L,R): 
+        if (i % M == 0): 
+            counter = counter + 1
+  
+    return counter 
+    print(countDivisibles(L,R,M)) 
+  
