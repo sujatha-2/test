21f1fc19e230d1466796484398c878814aab4ec8
sujatha-2 <sujatha16081998@gmail.com>
2018-10-15 17:29:41 +0530

    Create player set 5.41..py

---
+++ player set 5.41..py
@@ -0,0 +1,17 @@
+def check_Power(N,k):
+    if N <= 0 or k <=0:
+        print "not a valid input"
+    else:
+     for i in range (1,20):
+            x = k**i
+            if x == N :
+                print " True "
+                print k, "power ", i , "is", N
+                break
+            elif x> N:
+                print "false"
+                break
+                check_Power(244140625,5)
+check_Power(4096, 16)
+check_Power(0, 16)
+check_Power(1,1)
