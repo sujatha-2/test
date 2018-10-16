f5d6a118cb4559fd5b9d53113054b6c4961e9ee7
sujatha-2 <sujatha16081998@gmail.com>
2018-10-16 17:01:07 +0530

    Create player set5.47.py

---
+++ player set5.47.py
@@ -0,0 +1,7 @@
+def findAngle(n):
+ interiorAngle = int((n - 2) * 180 / n) 
+      print("Interior angle: " , 
+            interiorAngle )
+            n = 10
+            findAngle(n) 
+            
